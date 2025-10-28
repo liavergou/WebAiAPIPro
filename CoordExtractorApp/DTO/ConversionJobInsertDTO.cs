4f8d82ee713b1000779735843a7e1b38fc4c0c9f
liavergou <liaverg@gmail.com>
2025-10-28 22:59:26 +0200

    feat: add dto for conversion job request

---
+++ CoordExtractorApp/DTO/ConversionJobInsertDTO.cs
@@ -0,0 +1,13 @@
+﻿namespace CoordExtractorApp.DTO
+{
+    public class ConversionJobInsertDTO
+    {
+        public IFormFile File { get; set; } = null!;
+
+        public int ProjectId { get; set; }
+        public int PromptId { get; set; }
+
+        public Guid ClientRequestId { get; set; } //.net 8 αντι για uuid. (επειδή θα είναι async)
+
+    }
+}
