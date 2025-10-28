5fba1c13e69a2616283736937a3e53ad752e83b2
liavergou <liaverg@gmail.com>
2025-10-28 14:39:01 +0200

    feat: add encryptionutil for password validation

---
+++ CoordExtractorApp/Security/EncryptionUtil.cs
@@ -0,0 +1,21 @@
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace CoordExtractorApp.Security
+{
+    public static class EncryptionUtil
+    {
+        //κρυπτογράφηση password
+        public static string Encrypt(string clearText)
+        {
+            var encryptedPassword = BCrypt.Net.BCrypt.HashPassword(clearText);
+            return encryptedPassword;
+        }
+
+        //σύγκριση text χρηστη με encrypted password. one way function.
+        public static bool IsValidPassword(string plainText, string cipherText)
+        {
+            return BCrypt.Net.BCrypt.Verify(plainText, cipherText);
+        }
+    }
+}
+
