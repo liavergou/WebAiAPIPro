8277670343c2aba932ae9b96a14a90c5e3bfbcdb
liavergou <liaverg@gmail.com>
2025-10-30 06:31:10 +0200

    feat: add user repository

---
+++ CoordExtractorApp/Repositories/UserRepository.cs
@@ -0,0 +1,58 @@
+﻿using CoordExtractorApp.Data;
+using CoordExtractorApp.Models;
+using CoordExtractorApp.Security;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace CoordExtractorApp.Repositories
+{
+    public class UserRepository : BaseRepository<User>, IUserRepository
+    {
+        public UserRepository(TopoDbContext context) : base(context)
+        {
+        }
+
+        public async Task<User?> GetUserAsync(string username, string password)
+        {
+           var user = await context.Users
+                .FirstOrDefaultAsync(u=> u.Username == username);
+
+            if (user == null) return null;
+            if (!EncryptionUtil.IsValidPassword(password, user.Password)) return null;
+            return user;
+        }
+
+        public async Task<User?> GetUserByUsernameAsync(string username)
+        {
+            return await context.Users.FirstOrDefaultAsync(u => u.Username == username);
+        }
+
+        public async Task<PaginatedResult<User>> GetUsersAsync(int pageNumber, int pageSize,
+            List<Expression<Func<User, bool>>> predicates)
+        {
+            IQueryable<User> query = context.Users; // δεν εκτελείται
+
+            if (predicates != null && predicates.Count > 0)
+            {
+                foreach (var predicate in predicates)
+                {
+                    query = query.Where(predicate); // εκτελείται, υπονοείται το AND
+                }
+            }
+
+            int totalRecords = await query.CountAsync(); // εκτελείται
+
+            int skip = (pageNumber - 1) * pageSize;
+
+            var data = await query
+                .OrderBy(u => u.Id) // πάντα να υπάρχει ένα OrderBy πριν το Skip
+                .Skip(skip)
+                .Take(pageSize)
+                .ToListAsync(); // εκτελείται
+
+            return new PaginatedResult<User>(data, totalRecords, pageNumber, pageSize);
+        }
+
+
+    }
+    }
