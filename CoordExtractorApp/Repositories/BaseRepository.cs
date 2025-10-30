75f9ebfa251eda05486d014bc3404bc564a4c2dc
liavergou <liaverg@gmail.com>
2025-10-30 06:24:34 +0200

    feat: add base repository

---
+++ CoordExtractorApp/Repositories/BaseRepository.cs
@@ -0,0 +1,54 @@
+﻿using CoordExtractorApp.Data;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace CoordExtractorApp.Repositories
+{
+    public class BaseRepository<T> : IBaseRepository<T> where T : BaseEntity
+    {
+
+        protected readonly TopoDbContext context;
+        protected readonly DbSet<T> dbSet; //για να εφαρμοσθεί σε collection
+
+        //constructor
+        public BaseRepository(TopoDbContext context)
+        { 
+            this.context = context;
+            dbSet = context.Set<T>(); //dynamically retrieves DbSet
+        }
+
+        //η ίδια η add δεν είναι async αλλά πριν γίνει το commit πρέπει να επικοινωνήσει με τη βαση για να πάρει το τελευταίο id
+        public virtual async Task AddAsync(T entity) => await dbSet.AddAsync(entity);
+
+         
+        public virtual async Task AddRangeAsync(IEnumerable<T> entities) => await dbSet.AddRangeAsync(entities);
+
+        //δεν είναι async γιατί έχω ήδη φέρει το entity με get γι αυτό κάνω Attach
+        public virtual Task UpdateAsync(T entity)
+
+        {
+            dbSet.Attach(entity);
+            context.Entry(entity).State = EntityState.Modified;
+            return Task.CompletedTask; //είναι σαν void             
+        }
+
+
+        public virtual async Task<bool> DeleteAsync(int id)
+        {
+            T? existingEntity = await GetAsync(id);
+            if (existingEntity == null) return false;        
+            existingEntity.DeletedAt = DateTime.UtcNow;
+            existingEntity.ModifiedAt = DateTime.UtcNow;       
+            return true;
+
+        }
+        //IEnumerable - > iteratable ***τα deleted δεν πρεπει να εξαιρεθούν???
+        public virtual async Task<IEnumerable<T>> GetAllAsync() => await dbSet.ToListAsync();
+
+        //null αν δεν υπάρχει
+        public virtual async Task<T?> GetAsync(int id) => await dbSet.FindAsync(id);
+
+        public virtual async Task<int> GetCountAsync() => await dbSet.CountAsync();
+
+    }
+}
---
+++ CoordExtractorApp/Repositories/IBaseRepository.cs
@@ -0,0 +1,19 @@
+﻿using CoordExtractorApp.Data;
+
+namespace CoordExtractorApp.Repositories
+{
+    public interface IBaseRepository<T> where T : BaseEntity
+    {
+
+        //ΒΑΣΙΚΟ CRUD
+        Task AddAsync(T entity);
+        Task AddRangeAsync(IEnumerable<T> entities);
+        Task UpdateAsync(T entity);
+        Task<bool> DeleteAsync(int id);
+
+        //ΒΑΣΙΚΑ GET
+        Task<T?> GetAsync(int id);
+        Task<IEnumerable<T>> GetAllAsync();
+        Task<int> GetCountAsync();
+    }
+}
