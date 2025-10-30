ea3ef6f400be9c7eb091acd0049514ea2ee86cfa
liavergou <liaverg@gmail.com>
2025-10-30 08:07:13 +0200

    feat: add extensions methods to repositories

---
+++ CoordExtractorApp/Repositories/RepositoriesDIExtensions.cs
@@ -0,0 +1,12 @@
+﻿namespace CoordExtractorApp.Repositories
+{
+    public static class RepositoriesDIExtensions
+    {
+        public static IServiceCollection AddRepositories(this IServiceCollection services)
+        {
+            //επεκτείνουμε το functionality. προσθέτει το 
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
+            return services;
+        }
+    }
+}
