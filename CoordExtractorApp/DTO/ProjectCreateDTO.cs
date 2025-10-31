32134dc25d98789eac2d53c411b95c143532a452
liavergou <liaverg@gmail.com>
2025-10-31 20:43:07 +0200

    feat: add project create dto

---
+++ CoordExtractorApp/DTO/ProjectCreateDTO.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CoordExtractorApp.DTO
+{
+    public class ProjectCreateDTO
+    {
+        [Required(ErrorMessage = "The {0} field is required.")]
+        [StringLength(200, MinimumLength = 2, ErrorMessage = "Project name must be between 2 and 200 characters.")]
+        public string ProjectName { get; set; } = null!;
+
+        [StringLength(500, ErrorMessage = "Project description must be between 2 and 500 characters.")]
+        public string? Description { get; set; }
+
+    }
+}
