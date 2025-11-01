8adaa0a38f5520d80982a48d57c4f6d6ba4d9f93
liavergou <liaverg@gmail.com>
2025-11-01 11:43:51 +0200

    feat: add project dtos

---
+++ CoordExtractorApp/DTO/ProjectReadOnlyDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace CoordExtractorApp.DTO
+{
+    public class ProjectReadOnlyDTO
+    {
+        public int Id { get; set; }
+
+        public string ProjectName { get; set; } = null!;
+
+        public string? Description { get; set; }
+    }
+}
---
+++ CoordExtractorApp/DTO/ProjectUpdateDTO.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CoordExtractorApp.DTO
+{
+    public class ProjectUpdateDTO
+    {
+     
+        [StringLength(200, MinimumLength = 2, ErrorMessage = "Project name must be between 2 and 200 characters.")]
+        public string? ProjectName { get; set; } = null!;
+
+        [StringLength(500, ErrorMessage = "Project description must be between 2 and 500 characters.")]
+        public string? Description { get; set; }
+    }
+}
