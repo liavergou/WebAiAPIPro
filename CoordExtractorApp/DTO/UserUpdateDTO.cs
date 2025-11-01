fd666372a6475071a226769e9737e9815c06e6e6
liavergou <liaverg@gmail.com>
2025-11-01 11:24:45 +0200

    feat: add user update dto

---
+++ CoordExtractorApp/DTO/UserUpdateDTO.cs
@@ -0,0 +1,34 @@
+﻿using CoordExtractorApp.Core.Enums;
+using System.ComponentModel.DataAnnotations;
+
+namespace CoordExtractorApp.DTO
+{
+    public class UserUpdateDTO
+    {
+     
+        [StringLength(50, MinimumLength = 2, ErrorMessage = "Username must be between 2 and 50 characters.")]
+        public string? Username { get; set; }
+
+     
+        [StringLength(100, ErrorMessage = "Email must not exceed 100 characters.")]
+        [EmailAddress(ErrorMessage = "Invalid email address.")]
+        public string? Email { get; set; }
+
+        [RegularExpression(@"(?=.*?[A-Z])(?=.*?[a-z])(?=.*?\d)(?=.*?\W)^.{8,}$",
+            ErrorMessage = "Password must contain at least one uppercase, one lowercase, " +
+            "one digit, and one special character")]
+        public string? Password { get; set; }
+
+      
+        [StringLength(50, MinimumLength = 2, ErrorMessage = "Firstname must be between 2 and 50 characters.")]
+        public string? Firstname { get; set; }
+
+       
+        [StringLength(50, MinimumLength = 2, ErrorMessage = "Lastname must be between 2 and 50 characters.")]
+        public string? Lastname { get; set; }
+
+  
+        [EnumDataType(typeof(UserRole), ErrorMessage = "Invalid user role")]
+        public UserRole? UserRole { get; set; }
+    }
+}
