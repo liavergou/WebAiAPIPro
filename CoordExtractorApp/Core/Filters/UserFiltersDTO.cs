b630e51d262acd7ea9b771ade4465fada04ac25f
liavergou <liaverg@gmail.com>
2025-11-01 10:58:53 +0200

    feat: add user filter dto

---
+++ CoordExtractorApp/Core/Filters/UserFiltersDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace CoordExtractorApp.Core.Filters
+{
+    public class UserFiltersDTO
+    {
+        public string? Username { get; set; }
+        public string? UserRole { get; set; }
+    }
+}
