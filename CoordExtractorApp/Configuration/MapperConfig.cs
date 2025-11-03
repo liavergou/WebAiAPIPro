a67fb1e2745d980fdd7929619f762e22594ede98
liavergou <liaverg@gmail.com>
2025-11-03 19:52:58 +0200

    feat: add mappings for project dto and prompt dto

--- CoordExtractorApp/Configuration/MapperConfig.cs
+++ CoordExtractorApp/Configuration/MapperConfig.cs
@@ -12,12 +12,21 @@ namespace CoordExtractorApp.Configuration
             //αντιστοιχία απο το data model User προς το UserReadOnlyDTO
             CreateMap<User, DTO.UserReadOnlyDTO>()
                 .ReverseMap(); //αντίστροφη αντιστοίχιση
-            CreateMap<User, DTO.UserCreateDTO>()
-                .ReverseMap();
 
-            CreateMap<Project, DTO.ProjectCreateDTO>();
-            CreateMap<ConversionJob, DTO.ConversionJobInsertDTO>();
-           
+            CreateMap<DTO.UserCreateDTO, User>(); //απο controller προς service
+            CreateMap<DTO.UserUpdateDTO, User>(); //κάνω partial update. να μη το βαλω? ομοιως για τα αλλα update
+
+            CreateMap<Project, DTO.ProjectReadOnlyDTO>()
+                .ReverseMap(); //αντίστροφη αντιστοίχιση
+            CreateMap<DTO.ProjectCreateDTO, Project>();
+            CreateMap<DTO.ProjectUpdateDTO, Project>();
+
+            CreateMap<Prompt, DTO.PromptReadOnlyDTO>()
+                .ReverseMap(); //αντίστροφη αντιστοίχιση
+            CreateMap<DTO.PromptCreateDTO, Prompt>();
+            CreateMap<DTO.PromptUpdateDTO, Prompt>();
+
+
         }
 
     }
