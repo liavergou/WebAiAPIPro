1d86c8e578686c07300fa4c0eb60f732082cb37d
liavergou <liaverg@gmail.com>
2025-11-04 10:49:11 +0200

    feat(security): add keycloak jwt authentication and setup. add error handler middleware and keycloak role extentensions helper methods

---
+++ CoordExtractorApp/Helpers/KeycloakRoleExtensions.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using System.Security.Claims;
+using System.Text.Json;
+
+namespace CoordExtractorApp.Helpers
+{
+  
+        public static class KeycloakRoleExtensions
+        {           
+            public static Func<TokenValidatedContext, Task> MapKeycloakRolesToClaims() //event handler που διορθώνει τους ρόλους
+        {
+                return context =>
+                {
+                    var principal = context.Principal;
+                    if (principal?.Identity is ClaimsIdentity identity)
+                    {
+                        
+                        var realmAccessClaim = identity.FindFirst("realm_access"); // Ψάχνει το πεδίο claim 'realm_access' μεσα στο token
+
+                        if (realmAccessClaim != null)
+                        {
+                            var realmAccess = JsonDocument.Parse(realmAccessClaim.Value); //διαβάζει το json και κανει object τη τιμή για να μπορεί η c# να κάνει navigate μέσα στο json
+                            
+                            if (realmAccess.RootElement.TryGetProperty("roles", out var rolesElement) && rolesElement.ValueKind == JsonValueKind.Array) // Ψάχνει το array 'roles' μέσα στο realm_access object
+                            {
+                                foreach (var role in rolesElement.EnumerateArray())
+                                {
+                                    
+                                    identity.AddClaim(new Claim(ClaimTypes.Role, role.GetString()!)); // Προσθέτει κάθε ρόλο ως ClaimTypes.Role
+                                }
+                            }
+                        }
+                    }
+                    return Task.CompletedTask;
+                };
+            }
+        }
+    
+}
+//Το Keycloak βάζει τις πληροφορίες του ρόλου σε ένα περίπλοκο JSON πεδίο μέσα στο Token), που το ονομάζει realm_access.
+//Όμως η c# περιμένει να βρει τον ρόλο σε ένα πρότυπο πεδίο ClaimTypes.Role
--- CoordExtractorApp/Program.cs
+++ CoordExtractorApp/Program.cs
@@ -1,9 +1,17 @@
 
 using Azure.Identity;
+using CoordExtractorApp.Configuration;
+using CoordExtractorApp.Helpers;
 using CoordExtractorApp.Repositories;
+using CoordExtractorApp.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
-using Npgsql.EntityFrameworkCore.PostgreSQL;
+using Microsoft.IdentityModel.Tokens;
 using NetTopologySuite.IO;
+using Npgsql.EntityFrameworkCore.PostgreSQL;
+using Serilog;
+using System.Security.Claims;
+
 
 namespace CoordExtractorApp
 {
@@ -21,59 +29,111 @@ namespace CoordExtractorApp
                 builder.Configuration.AddAzureKeyVault(new Uri(keyVaultUri), new DefaultAzureCredential());
             }
 
+            //generativeAI (gemini key)
             var geminiApiKey = builder.Configuration["Gemini:Credentials:ApiKey"];
 
             // **CONNECTION STRING**
             var connString = builder.Configuration.GetConnectionString("DefaultConnection");
 
             // **SERVICES ΣΤΟ DI CONTAINER**
+        
+            //POSTGIS
             builder.Services.AddDbContext<CoordExtractorApp.Data.TopoDbContext>(options =>
-            options.UseNpgsql(connString));
-            //για το postgis
-            builder.Services.AddDbContext<CoordExtractorApp.Data.TopoDbContext>(options =>
-            options.UseNpgsql(connString, o => o.UseNetTopologySuite())
-);
-
+            options.UseNpgsql(connString, o => o.UseNetTopologySuite()));
+                        
             //Repositories & Unit of Work (μέσω της Extension Method)
             builder.Services.AddRepositories();
-            builder.Services.AddScoped<IUserRepository, UserRepository>(); // Καταχώρηση UserRepository
 
-
-            // Services (για το dependency injection
+            // Services (για το dependency injection)
+            builder.Services.AddScoped<IUserService, UserService>();
+            builder.Services.AddScoped<IPromptService, PromptService>();
 
             // AutoMapper
+            builder.Services.AddAutoMapper(cfg => cfg.AddProfile<MapperConfig>());
+            builder.Host.UseSerilog((ctx, lc) =>
+                lc.ReadFrom.Configuration(ctx.Configuration));
 
-            //**AUTHENTICATION ME JWT**
+            //**AUTHENTICATION ME KEYCLOAK**            
+            builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme) //ρυθμιση services και ρυθμιση middleware
+                .AddJwtBearer(options =>
+                {                    
+                    options.Authority = "http://localhost:8080/realms/TopoApp"; //ρύθμιση του keycloak server
+                    options.Audience = "react-app"; //ποιος χρησιμοποιεί το token                    
+                    options.RequireHttpsMetadata = false; //για να αγνοήσει το https dev mode
+                    
+                    options.TokenValidationParameters = new TokenValidationParameters //ρύθμιση για την αναγνωση των roles
+                    {
+                        ValidateIssuer = true, //απενεργοποιεί τον έλεγχο του issuer γιατι το κανει το authority
+                        RoleClaimType = ClaimTypes.Role //πως να διαβάσει τους ρόλους απο το token.
+                    };
+
+                    options.Events = new JwtBearerEvents
+                    {
+                        OnTokenValidated = KeycloakRoleExtensions.MapKeycloakRolesToClaims()
+                    };
+                });
 
             //**CORS**
+            builder.Services.AddCors(options =>
+            {
+                options.AddPolicy("ReactClient",
+                    b => b.WithOrigins(
+                        "http://localhost:5173",
+                        "http://localhost:5174",
+                        "http://localhost:5175",
+                        "http://localhost:5176"
+                        )
+                        .AllowAnyMethod()
+                        .AllowAnyHeader()
+                );
+            });
+            //Μονο για Testing!!!
+            builder.Services.AddCors(options =>
+            {
+                options.AddPolicy("LocalClient",
+                    b => b.WithOrigins("https://localhost:5001")
+                        .AllowAnyMethod()
+                        .AllowAnyHeader()
+                );
+            });
+
+            //builder.Services.AddCors(options =>
+            //{
+            //    options.AddPolicy("AllowAll",
+            //        b => b.AllowAnyOrigin()
+            //            .AllowAnyMethod()
+            //            .AllowAnyHeader()
+            //    );
+            //});
+
+
+
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
 
+
+            //--------------------------------------------------------------
             var app = builder.Build();
-
-            //KEY VAULT TEST CONNECTION
-            app.MapGet("/kv-test", (IConfiguration cfg) =>
-            {
-                //var val = cfg["ConnectionStrings:DefaultConnection"];
-                //return Results.Ok(new { hasValue = !string.IsNullOrEmpty(val) });
-                var safeConn = connString?.Replace("Password=", "Password=***");
-                return Results.Ok(new { connectionString = safeConn });
-            });
-
-        
+      
             // Configure the HTTP request pipeline.
+
             if (app.Environment.IsDevelopment())
             {
                 app.UseSwagger();
-                app.UseSwaggerUI();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "CoordExtractor App v1"));
             }
 
             app.UseHttpsRedirection();
 
+            app.UseCors("LocalClient");
+
+            app.UseAuthentication();
+
             app.UseAuthorization();
 
+            app.UseMiddleware<ErrorHandlerMiddleware>(); //οπως στο schoolApp αλλά χωρίς το registration error handler
 
             app.MapControllers();
 
