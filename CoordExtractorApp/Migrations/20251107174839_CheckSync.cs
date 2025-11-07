4cc51bd8bedd5edead799a1283df27d567aef8af
liavergou <liaverg@gmail.com>
2025-11-07 20:17:32 +0200

    chore: sync user entity changes for keycloak

---
+++ CoordExtractorApp/Migrations/20251107174839_CheckSync.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CoordExtractorApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class CheckSync : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "KeycloakId",
+                table: "Users",
+                type: "character varying(36)",
+                maxLength: 36,
+                nullable: false,
+                oldClrType: typeof(Guid),
+                oldType: "uuid",
+                oldMaxLength: 36);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<Guid>(
+                name: "KeycloakId",
+                table: "Users",
+                type: "uuid",
+                maxLength: 36,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(36)",
+                oldMaxLength: 36);
+        }
+    }
+}
