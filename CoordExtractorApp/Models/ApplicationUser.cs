1d5ddc3344d6eb302b5491c0a94dbbe4a71b6c85
liavergou <liaverg@gmail.com>
2025-11-07 20:08:39 +0200

    feat: add application user model

---
+++ CoordExtractorApp/Models/ApplicationUser.cs
@@ -0,0 +1,18 @@
+﻿namespace CoordExtractorApp.Models
+{
+    public class ApplicationUser
+    {
+        public int? Id { get; set; } //της βάσης
+
+        public string? KeycloakId { get; set; } = string.Empty; //του token
+        public string? Username { get; set; } = string.Empty; //του token
+
+        public string? Email {  get; set; } = string.Empty; //από το token
+
+        public string? Lastname {  get; set; } = string.Empty; //απο το token
+
+        public string? Firstname {  get; set; } = string.Empty; //απο το token
+
+        public string? Role { get; set; } = string.Empty; //απο το token αφου γινει mapping. ClaimTypes.Role από το keycloak
+    }
+}
