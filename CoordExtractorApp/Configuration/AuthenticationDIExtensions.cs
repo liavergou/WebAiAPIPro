325141f69fd9c90cb84fa1c595f8572f253bb7bc
liavergou <liaverg@gmail.com>
2025-11-16 10:45:04 +0200

    chore: remove valid issuer and valid audience from token validation parameters. included in options.

--- CoordExtractorApp/Configuration/AuthenticationDIExtensions.cs
+++ CoordExtractorApp/Configuration/AuthenticationDIExtensions.cs
@@ -7,9 +7,9 @@ namespace CoordExtractorApp.Configuration
     {
 
         //Επέκταση του IServiceCollection με Keycloak JWT Authantication configuration
+        //https://www.keycloak.org/docs-api/latest/rest-api/index.html
         // https://dev.to/kayesislam/integrating-openid-connect-to-your-application-stack-25chservices
         //https://learn.microsoft.com/en-us/dotnet/api/microsoft.aspnetcore.authentication.jwtbearer.jwtbeareroptions?view=aspnetcore-8.0
-
         public static IServiceCollection AddKeycloakAuthentication
             (this IServiceCollection services, IConfiguration configuration)
         {
@@ -18,16 +18,14 @@ namespace CoordExtractorApp.Configuration
                 {
                     options.Authority = configuration["Keycloak:Authority"]; //ρύθμιση του keycloak server. κατεβαζει το αρχειο metadata στο endpoint. παιρνει τις διευθυνσεις για τα keys για την κρυπτογράφηση. Την επίσημη τιμή για τον Issuer. Γι αυτο δεν βάζω ValidIssuer
                     options.Audience = configuration["Keycloak:Audience"]; //ποιος χρησιμοποιεί το token                    
-                    options.RequireHttpsMetadata = false; //για να αγνοήσει το https dev mode
+                    options.RequireHttpsMetadata = false; //για να αγνοήσει το https (dev mode)
 
-                    options.MapInboundClaims = false;
+                    options.MapInboundClaims = false; //απενεργοποιηση της αυτόματης μετονομασίας των claims για να χρησιμοποιήσω τα ονόματα όπως τα βλέπω στο JWT
 
                     options.TokenValidationParameters = new TokenValidationParameters //ρύθμιση για την αναγνωση των roles
                     {
-                        ValidIssuer = configuration["Keycloak:Audience"],
                         ValidateIssuer = true, //ενεργοποιεί τον έλεγχο του issuer. λογω του .Authority, η .net κατεβαζει την τιμή του issuer απο τα metadata
                         ValidateAudience = true,
-                        ValidAudience = configuration["Keycloak:Audience"],
                         RoleClaimType = "role" //πως να διαβάσει τους ρόλους απο το token.
                     };
                 });
