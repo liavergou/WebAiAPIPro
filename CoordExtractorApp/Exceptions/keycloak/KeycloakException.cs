fc86c9bfeec33809b7e6d4bad3a84eeafa9362e8
liavergou <liaverg@gmail.com>
2025-11-17 21:06:34 +0200

    feat: add keycloak exception for keycloak service errors

---
+++ CoordExtractorApp/Exceptions/keycloak/KeycloakException.cs
@@ -0,0 +1,13 @@
+﻿namespace CoordExtractorApp.Exceptions.keycloak
+{
+    public class KeycloakException : Exception
+    {
+        public string ServiceName { get; }
+
+        public KeycloakException(string serviceName, string message)
+            : base(message)
+        {
+            ServiceName = serviceName;
+        }
+    }
+}
