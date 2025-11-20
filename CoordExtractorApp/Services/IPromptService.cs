b73ce459f4c371d154fbdf801fe549028a1000c8
liavergou <liaverg@gmail.com>
2025-11-20 23:16:16 +0200

    feat: add get all prompts async and create prompt async methods. implement dto mapping in service layer

--- CoordExtractorApp/Services/IPromptService.cs
+++ CoordExtractorApp/Services/IPromptService.cs
@@ -5,17 +5,22 @@ namespace CoordExtractorApp.Services
 {
     public interface IPromptService
     {
-
+        //read
         Task<PromptReadOnlyDTO?> GetPromptByIdAsync(int id);
         
         Task<PromptReadOnlyDTO?> GetPromptByPromptNameAsync(string promptName);
 
         Task<PaginatedResult<PromptReadOnlyDTO>> GetPaginatedPromptsAsync(int page, int pageSize);
 
-        Task CreatePromptAsync(PromptCreateDTO request);
+        Task<List<PromptReadOnlyDTO>> GetAllPromtsAsync();
 
+        //create
+        Task<PromptReadOnlyDTO>CreatePromptAsync(PromptCreateDTO promptCreateDTO);
+
+        //update
         Task<bool> UpdatePromptAsync(int id, PromptUpdateDTO promptupdatedto);
 
+        //delete
         Task<bool> DeletePromptAsync(int id);
     }
 }
--- CoordExtractorApp/Services/PromptService.cs
+++ CoordExtractorApp/Services/PromptService.cs
@@ -14,7 +14,7 @@ namespace CoordExtractorApp.Services
     {
         private readonly IUnitOfWork unitOfWork;
         private readonly IMapper mapper;
-        private readonly ILogger<PromptService> logger = 
+        private readonly ILogger<PromptService> logger =
             new LoggerFactory().AddSerilog().CreateLogger<PromptService>();
 
 
@@ -37,16 +37,18 @@ namespace CoordExtractorApp.Services
                     throw new EntityNotFoundException("Prompt", "Prompt with id: " + id + " not found.");
                 }
                 logger.LogInformation("Prompt found with ID: {Id}", id);
-                return mapper.Map<PromptReadOnlyDTO>(prompt);
+                var dto = mapper.Map<PromptReadOnlyDTO>(prompt);
+
+                return dto;
 
             }
             catch (EntityNotFoundException ex)
             {
-                logger.LogError("Error retrieving prompt by ID: {Id}. {Message}", id, ex.Message);
+                logger.LogError("Error retrieving prompt with ID: {Id}. {Message}", id, ex.Message);
                 throw;
 
             }
-            
+
         }
 
         public async Task<PromptReadOnlyDTO?> GetPromptByPromptNameAsync(string promptName)
@@ -59,8 +61,9 @@ namespace CoordExtractorApp.Services
                     throw new EntityNotFoundException("Prompt", "Prompt with name: " + promptName + " not found.");
                 }
                 logger.LogInformation("Prompt found:{PromptName}", promptName);
-                
-                return mapper.Map<PromptReadOnlyDTO>(prompt);
+
+                var dto = mapper.Map<PromptReadOnlyDTO>(prompt);
+                return dto;
 
             }
             catch (EntityNotFoundException ex)
@@ -70,23 +73,33 @@ namespace CoordExtractorApp.Services
             }
         }
 
+        public async Task<List<PromptReadOnlyDTO>> GetAllPromtsAsync()
+        {
+            var prompts = await unitOfWork.PromptRepository.GetAllAsync();
+            var dto = mapper.Map<List<PromptReadOnlyDTO>>(prompts)
+                .OrderBy(p => p.Id)
+                .ToList();
+            logger.LogInformation("Retrieved all prompts. Count:{Count}", dto.Count);
+            return dto;
+        }
+
         //χωρις φίλτρο
         public async Task<PaginatedResult<PromptReadOnlyDTO>> GetPaginatedPromptsAsync(int pageNumber, int pageSize)
         {
             try
             {   //καλώ repository χωρις λιστα πλεον
                 var prompts = await unitOfWork.PromptRepository.GetPaginatedPromptsAsync(pageNumber, pageSize);
-                //Data = mapper.Map<List<PromptReadOnlyDTO>>(prompts.Data),
+
                 var result = new PaginatedResult<PromptReadOnlyDTO>
                 {
-
+                    //custom για εξασκηση
                     Data = prompts.Data.Select(p => new PromptReadOnlyDTO
                     {
                         Id = p.Id,
                         PromptName = p.PromptName,
                         PromptText = p.PromptText
                     }).ToList(),
-                    
+
                     TotalRecords = prompts.TotalRecords,
                     PageNumber = prompts.PageNumber,
                     PageSize = prompts.PageSize
@@ -100,54 +113,123 @@ namespace CoordExtractorApp.Services
             }
         }
 
-        public Task CreatePromptAsync(PromptCreateDTO request)
+        public async Task<PromptReadOnlyDTO> CreatePromptAsync(PromptCreateDTO promptCreateDTO)
         {
-            throw new NotImplementedException();
+            try
+            {
+                var existingPrompt = await unitOfWork.PromptRepository.GetPromptByPromptNameAsync(promptCreateDTO.PromptName);
+                if (existingPrompt != null)
+                {
+                    throw new EntityAlreadyExistsException("Prompt", $"Prompt with name {promptCreateDTO.PromptName} already exists");
+                }
+
+                //dto -> entity
+                var prompt = mapper.Map<Prompt>(promptCreateDTO);
+
+
+                await unitOfWork.PromptRepository.AddAsync(prompt);
+
+                await unitOfWork.SaveAsync(); //commit
+
+                //entity->dto
+                var dto = mapper.Map<PromptReadOnlyDTO>(prompt);
+                logger.LogInformation("Prompt with ID {id} created successfully", dto.Id);
+                return dto;
+            }
+            catch (AppException ex)
+            {
+                logger.LogError("Error creating prompt.Code {Code}, Message: {Message}", ex.Code, ex.Message);
+                throw;
+            }
         }
 
-        public async Task<bool> UpdatePromptAsync(int id, PromptUpdateDTO promptupdatedto)
+
+
+        public async Task<bool> UpdatePromptAsync(int id, PromptUpdateDTO promptUpdateDTO)
+        {
+            try
+            {
+                var prompt = await unitOfWork.PromptRepository.GetAsync(id);
+                //αν δεν υπάρχει
+                if (prompt == null)
+                {
+                    throw new EntityNotFoundException("Prompt", $"Prompt with id: {id} not found");
+                }
+                if (!string.IsNullOrEmpty(promptUpdateDTO.PromptName) && promptUpdateDTO.PromptName != prompt.PromptName)
+                {
+                    //promptName unique. και ελεγχος αν υπάρχει ήδη
+                    var existingPrompt = await unitOfWork.PromptRepository.GetPromptByPromptNameAsync(promptUpdateDTO.PromptName!);
+
+                if (existingPrompt != null && existingPrompt.Id != id)
+                    {
+                        throw new EntityAlreadyExistsException("Prompt", $"Prompt with name {promptUpdateDTO.PromptName} already exists.");
+                    }
+                    //update PromptName
+                    prompt.PromptName = promptUpdateDTO.PromptName;
+                   
+                }
+                //update PromptText
+                if (promptUpdateDTO.PromptText != null)
+                    prompt.PromptText = promptUpdateDTO.PromptText;
+
+                await unitOfWork.SaveAsync();
+
+                logger.LogInformation("Prompt with {id} updated successfully", id);
+                return true;
+
+            }
+            catch (EntityNotFoundException ex)
+            {
+                logger.LogError("Error retrieving prompt with id {id}. {Message}", id, ex.Message);
+                throw;
+            }
+            catch (EntityAlreadyExistsException ex)
+            {
+                logger.LogError("Error updating prompt with id {id}. {Message}", id, ex.Message);
+                throw;
+            }
+
+
+
+        }
+
+        public async Task<bool> DeletePromptAsync(int id)
         {
             try
             {
                 var prompt = await unitOfWork.PromptRepository.GetAsync(id);
                 if (prompt == null)
                 {
-                    throw new EntityNotFoundException("Prompt", $"Prompt with id: {id} not found");
+                    throw new EntityNotFoundException("Prompt", $"Prompt with id {id} not found");
                 }
-                //partial update
-                if (promptupdatedto.PromptName != null) prompt.PromptName = promptupdatedto.PromptName;
-                if (promptupdatedto.PromptText != null) prompt.PromptText = promptupdatedto.PromptText;
+
+                //αν υπάρχουν conversion jobs συνδεδεμένα -> warning για cascade soft delete
+                var jobs = await unitOfWork.ConversionJobRepository.GetJobsByPromptIdAsync(id);
+
+                if (jobs.Count>0)
+                {
+                    logger.LogWarning("Prompt with {id} and {PromptText} has {JobCount} jobs assigned. Cascading soft delete", id, prompt.PromptText, jobs.Count);
+                }
+                //iteration στα jobs
+                foreach (var job in jobs)
+                {
+                    await unitOfWork.ConversionJobRepository.DeleteAsync(job.Id);
+                }
+                await unitOfWork.PromptRepository.DeleteAsync(id);
 
                 await unitOfWork.SaveAsync();
-                logger.LogInformation("Prompt with {id} updated succesfully", id);
+
+                logger.LogInformation("Prompt with {id} deleted successfully. Deleted {jobCount} jobs", id, jobs.Count);
+
                 return true;
-            } catch (EntityNotFoundException ex) {
-                logger.LogError("Error updating prompt {id}. {Message}", id, ex.Message);
-                throw;
+
             }
-        }
-        public async Task<bool> DeletePromptAsync(int id)
-        {
-            try
-            {
-                bool deletedPrompt = await unitOfWork.PromptRepository.DeleteAsync(id);
-                if (!deletedPrompt)
-                {
-                    throw new EntityNotFoundException("Prompt", $"Prompt with id: {id} not found");
-                }
-                await unitOfWork.SaveAsync();
-                logger.LogInformation("Prompt with {id} deleted successfully", id);
-                return true;
-            
-            }catch (EntityNotFoundException ex)
+            catch (EntityNotFoundException ex)
             {
                 logger.LogError("Error deleting prompt with {id}. {Message}", id, ex.Message);
                 throw;
             }
-        }        
-             
-       
+        }
     }
-   
 
-}
+    }
