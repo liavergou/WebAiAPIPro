69a721961ea9058f91f945b06ce7f5b2c145d3e7
liavergou <liaverg@gmail.com>
2025-11-20 22:59:38 +0200

    feat: add project service interface

---
+++ CoordExtractorApp/Services/IProjectService.cs
@@ -0,0 +1,27 @@
+﻿using CoordExtractorApp.Core.Filters;
+using CoordExtractorApp.Data;
+using CoordExtractorApp.DTO;
+using CoordExtractorApp.Models;
+
+namespace CoordExtractorApp.Services
+{
+    public interface IProjectService
+    {
+        //read
+        Task<ProjectReadOnlyDTO?> GetProjectByIdAsync(int id);
+        Task<ProjectReadOnlyDTO?> GetProjectByProjectNameAsync(string projectName);
+        Task<List<ProjectReadOnlyDTO>> GetAllProjectsAsync();
+        Task<PaginatedResult<ProjectReadOnlyDTO>> GetPaginatedProjectsAsync(int pageNumber, int pageSize);        
+
+        //create
+        Task<ProjectReadOnlyDTO> CreateProjectAsync(ProjectCreateDTO projectCreateDTO);
+
+        //update
+        Task<bool> UpdateProjectAsync(int id, ProjectUpdateDTO projectUpdateDTO);
+
+        //delete
+        Task<bool> DeleteProjectAsync(int id);
+
+
+    }
+}
