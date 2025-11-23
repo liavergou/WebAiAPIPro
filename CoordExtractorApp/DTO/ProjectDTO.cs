224c438ce2f4bd824c1a83d9efe21e4a35b13796
liavergou <liaverg@gmail.com>
2025-11-23 04:37:42 +0200

    feat: add project dto (jobs count included)

---
+++ CoordExtractorApp/DTO/ProjectDTO.cs
@@ -0,0 +1,14 @@
+﻿namespace CoordExtractorApp.DTO
+{
+    //περιλαμβάνει το count των jobs
+    public class ProjectDTO
+    {
+        public int Id { get; set; }
+
+        public string ProjectName { get; set; } = null!;
+
+        public string? Description { get; set; }
+
+        public int JobsCount { get; set; }
+    }
+}
