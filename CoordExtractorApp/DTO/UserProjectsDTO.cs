288ec1a7e08836325546b0cfe4d2261127486644
liavergou <liaverg@gmail.com>
2025-11-27 08:28:58 +0200

    feat: add user projects dto and user projects update dto to handle user-projects permissions

---
+++ CoordExtractorApp/DTO/UserProjectsDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace CoordExtractorApp.DTO
+{
+    public class UserProjectsDTO
+    {
+        //για GET /api/users/{id}/projects
+        public int Id {  get; set; }
+        public List<int> ProjectIds { get; set; } = []; //τα ήδη assigned project ids
+    }
+}
---
+++ CoordExtractorApp/DTO/UserProjectsUpdateDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace CoordExtractorApp.DTO
+{
+    public class UserProjectsUpdateDTO
+    {
+        //για PUT /api/users/{id}/projects
+        public int Id { get; set; }
+        public List<int> ProjectIds { get; set; } = []; //τα checked projects
+    }
+}
