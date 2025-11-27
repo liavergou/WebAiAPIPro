33b5b7ecbfbc5f24cb2b5cdf29cb0822c4f529c8
liavergou <liaverg@gmail.com>
2025-11-27 11:26:41 +0200

    feat: add user-projects assignment service and endpoints

---
+++ CoordExtractorApp/Services/IUserProjectsService.cs
@@ -0,0 +1,11 @@
+﻿using CoordExtractorApp.DTO;
+
+namespace CoordExtractorApp.Services
+{
+    public interface IUserProjectsService
+    {
+        Task<UserProjectsDTO> GetUserProjectsAsync(int id); //να επιστρέψει τη λίστα με τα ids των project απο τη βάση
+
+        Task <UserProjectsDTO> UpdateUserProjectsAsync(int id, UserProjectsUpdateDTO dto);
+    }
+}
---
+++ CoordExtractorApp/Services/UserProjectsService.cs
@@ -0,0 +1,78 @@
+﻿using AutoMapper;
+using CoordExtractorApp.Data;
+using CoordExtractorApp.DTO;
+using CoordExtractorApp.Exceptions;
+using CoordExtractorApp.Repositories;
+using CoordExtractorApp.Services.Keycloak;
+using Microsoft.Identity.Client;
+using Serilog;
+
+namespace CoordExtractorApp.Services
+{
+    public class UserProjectsService : IUserProjectsService
+    {
+        private readonly IUnitOfWork unitOfWork;
+        private readonly ILogger<UserProjectsService> logger =
+            new LoggerFactory().AddSerilog().CreateLogger<UserProjectsService>();
+
+        public UserProjectsService(IUnitOfWork unitOfWork,ILogger<UserProjectsService> logger)
+        {
+            this.unitOfWork = unitOfWork;
+        }
+
+        public async Task<UserProjectsDTO>GetUserProjectsAsync(int id)
+        {
+            User? user = null;
+            try
+            {
+                user = await unitOfWork.UserRepository.GetAsync(id);
+                if (user == null)
+                {
+                    throw new EntityNotFoundException("User", $"User with id: {id} not found");
+                }
+
+                var assignedProjectIds = await unitOfWork.UserRepository.GetProjectIdsForUserAsync(id);
+                var dto = new UserProjectsDTO
+                {
+                    ProjectIds = assignedProjectIds
+                };
+
+                logger.LogInformation("Retrieved {Count} projects for user {id}", id,assignedProjectIds.Count);
+
+                return  dto;
+            }
+            catch (EntityNotFoundException ex)
+            {
+                logger.LogError("Error retrieving user by ID: {Id}. {Message}", id, ex.Message);
+                throw;
+            }        
+        }
+ 
+        public async Task<UserProjectsDTO> UpdateUserProjectsAsync(int id, UserProjectsUpdateDTO dto)
+        {
+            try
+            {
+                var user = await unitOfWork.UserRepository.GetAsync(id);
+
+                if (user == null)
+                {
+                    throw new EntityNotFoundException("User", $"User with id: {id} not found");
+                }
+                await unitOfWork.UserRepository.SetProjectsForUserAsync(id, dto.ProjectIds);
+
+                await unitOfWork.SaveAsync();
+
+                logger.LogInformation($"Update {dto.ProjectIds.Count} projects for user {id}.", id, dto.ProjectIds.Count);
+                return await GetUserProjectsAsync(id); //επιστροφή των id των project. θα μπορουσα να κανω και list με project....
+            }
+
+            catch (EntityNotFoundException ex)
+            {
+                logger.LogError(
+                    "Error updating projects for user {id}.{Message}", id, ex.Message);
+                throw;
+
+            }
+        }
+    }
+}
