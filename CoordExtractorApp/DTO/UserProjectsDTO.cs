31eded5d367d6f4738ccab87d4b6cfbce3126521
liavergou <liaverg@gmail.com>
2025-11-27 11:22:31 +0200

    refactor: removed id property from user projects dto. user id is located in url.

--- CoordExtractorApp/DTO/UserProjectsDTO.cs
+++ CoordExtractorApp/DTO/UserProjectsDTO.cs
@@ -3,7 +3,6 @@
     public class UserProjectsDTO
     {
         //για GET /api/users/{id}/projects
-        public int Id {  get; set; }
-        public List<int> ProjectIds { get; set; } = []; //τα ήδη assigned project ids
+        public List<int> ProjectIds { get; set; } = []; //τα assigned project ids
     }
 }
--- CoordExtractorApp/DTO/UserProjectsUpdateDTO.cs
+++ CoordExtractorApp/DTO/UserProjectsUpdateDTO.cs
@@ -1,9 +1,11 @@
-﻿namespace CoordExtractorApp.DTO
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CoordExtractorApp.DTO
 {
     public class UserProjectsUpdateDTO
     {
         //για PUT /api/users/{id}/projects
-        public int Id { get; set; }
+        [Required(ErrorMessage = "Projects list is required, filled or empty")]
         public List<int> ProjectIds { get; set; } = []; //τα checked projects
     }
 }
