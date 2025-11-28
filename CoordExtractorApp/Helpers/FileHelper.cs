b80d1b318c1fcb8acde095da5d694eda470a1029
liavergou <liaverg@gmail.com>
2025-11-28 09:40:32 +0200

    feat: add functionallity to store image cropped files to local file system. add static helper method save image from bytes async

---
+++ CoordExtractorApp/Helpers/FileHelper.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+
+namespace CoordExtractorApp.Helpers
+{
+    public class FileHelper
+    {
+
+        public static async Task<string> SaveImageFromBytesAsync(byte[] imageBytes, string originalFileName, int projectId, IConfiguration configuration)
+
+        {
+            var storagePath = configuration["StoragePaths:Images"]; //από appsettings
+
+            //έλεγψος αν υπάρχει το storage path
+            if (storagePath == null)
+            {
+                throw new InvalidOperationException("Storage path for images is not configured.");
+            }
+
+            //δημιουργία subfolder για κάθε project πχ. Project_1 κλπ
+            var projectPath = Path.Combine(storagePath, $"Project_{projectId}");
+            Directory.CreateDirectory(projectPath);
+
+            //είδα και τα ticks αλλα λεει είναι not thread safe. Προσοχή ή θα επιλέξω {extension} ή παω απευθειας για png
+            var extension = Path.GetExtension(originalFileName);
+            var uniqueFileName = $"{Guid.NewGuid()}{extension}";
+
+            //Full Path
+            var fullPath = Path.Combine(projectPath, uniqueFileName);
+
+            //save στον δίσκο
+            await File.WriteAllBytesAsync(fullPath, imageBytes);
+
+            //για αποθηκευση στη βαση
+            return uniqueFileName;
+
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
