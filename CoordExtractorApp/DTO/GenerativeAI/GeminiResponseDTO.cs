5f6f36424065417936d3f547d15c26589ee5daa6
liavergou <liaverg@gmail.com>
2025-11-28 09:31:39 +0200

    feat: add dto for gemini api response

---
+++ CoordExtractorApp/DTO/GenerativeAI/GeminiResponseDTO.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.Json.Serialization;
+
+namespace CoordExtractorApp.DTO.GenerativeAI
+{
+    public class GeminiResponseDTO
+    {
+        [JsonPropertyName("wktOutput")] //ποιο πεδίο του json είναι
+        public string WktPolygon { get; set; } = "";
+
+        [JsonPropertyName("pointCount")]
+        public int PointCount { get; set; }
+
+        [JsonPropertyName("errors")]
+        public List<string> Errors { get; set; } = [];
+    }
+}
