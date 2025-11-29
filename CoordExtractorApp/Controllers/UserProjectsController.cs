d8503ee958f23f543657b5903ff4d5b6639f174c
liavergou <liaverg@gmail.com>
2025-11-29 22:18:47 +0200

    feat: add endpoint to get logged in user's projects

---
+++ CoordExtractorApp/Controllers/UserProjectsController.cs
@@ -0,0 +1,41 @@
+﻿using CoordExtractorApp.DTO;
+using CoordExtractorApp.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoordExtractorApp.Controllers
+{
+    [ApiController]
+    [Route("api/account")]
+    public class UserProjectsController : BaseController
+    {
+        public UserProjectsController(IApplicationService applicationService) : base(applicationService)
+        {
+
+        }
+
+        //GET USER PROJECTS FOR LOGGED IN USER
+        //GET /api/account/projects
+        [HttpGet("projects")]
+        [Authorize]
+        [ProducesResponseType(typeof(List<ProjectReadOnlyDTO>), 200)]
+        //[ProducesResponseType(401)] //unautorized
+
+        public async Task<IActionResult> GetAssignedProjects()
+        {
+            //Ο user που συνδέεται με το conversion job
+            var user = await GetUserInfoAsync(); //(base) για τον current user
+
+            if (user.Id == null)
+            {
+                return StatusCode(401, "User is null");
+            }
+
+            var projects = await applicationService.UserProjectsService.GetUserProjectsByUserIdAsync(user.Id.Value);
+            
+            return Ok(projects);
+        }
+
+
+    }
+}
