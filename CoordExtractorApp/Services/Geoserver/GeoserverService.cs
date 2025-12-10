e945e3c106d7e828d069af2dadc2ace1d159342e
liavergou <liaverg@gmail.com>
2025-12-10 19:52:58 +0200

    refactor: update cql filter. move deleted polygonsfiltering to geoserver sql view

--- CoordExtractorApp/Services/Geoserver/GeoserverService.cs
+++ CoordExtractorApp/Services/Geoserver/GeoserverService.cs
@@ -21,11 +21,12 @@ namespace CoordExtractorApp.Services.Geoserver
             string baseUrl = configuration["Geoserver:BaseUrl"] ?? throw new InvalidOperationException("Geoserver:BaseUrl configuration is missing."); ;
             string typeName = configuration["Geoserver:ConversionJobsLayer"] ?? throw new InvalidOperationException("Geoserver:ConversionJobsLayer configuration is missing."); ;
 
-            string cqlFilter = $"ProjectId={projectId} AND DeletedAt IS NULL";
+            string cqlFilter = $"ProjectId={projectId}"; //το DeletedAt IS NULL μεταφερθηκε στο sql του view του geoserver
 
             string encodedCqlFilter = Uri.EscapeDataString(cqlFilter); //το 20% δεν δουλεψε
 
-            //http://localhost:8085/geoserver/wfs?service=WFS&request=GetFeature&typeName=topo_app:ConversionJobs&outputFormat=application/json&srsName=EPSG:4326 + το φίλτρο
+            //http://localhost:8085/geoserver/wfs?service=WFS&request=GetFeature&typeName=topo_app:ConversionJobsView&outputFormat=application/json&srsName=EPSG:4326 + το φίλτρο
+            //http://localhost:8085/geoserver/wfs?service=WFS&request=GetFeature&typeName=topo_app:ConversionJobsView&outputFormat=application/json&srsName=EPSG:4326&cql_filter=ProjectId%3D21
             string url = $"{baseUrl}?service=WFS&request=GetFeature&typeName={typeName}&outputFormat=application/json&srsName=EPSG:4326&cql_filter={encodedCqlFilter}";
 
             //διορθωση σε δημιουργία client instance απο το factory για καθε request. δεν ήταν σωστό να χρησιμοποιώ τον ίδιο HttpClient. πιθανό Mix στα credentials σε δδιαφορετικές κλήσεις
